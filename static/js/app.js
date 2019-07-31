b89c8f4dac2eff532ddc56100195e9c460c64949
chienyuanhung <chien.yuanhung@gmail.com>
2019-07-31 13:51:12 -0700

    bulld gauge

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,6 @@ function buildMetadata(sample) {
   // Use `d3.json` to fetch the metadata for a sample
   d3.json(url1).then(function(metadata){
     var metadata = [metadata];
-    console.log(metadata);
 
     // Use d3 to select the panel with id of `#sample-metadata`
     var dataContainer = d3.select("#sample-metadata")
@@ -16,8 +15,8 @@ function buildMetadata(sample) {
     Object.entries(metadata).forEach(([key, value])=>{
       dataPair.push(key,value);
     });
-    pair = Object.entries(dataPair[1])
-    
+    pair = Object.entries(dataPair[1]);
+    console.log(pair[5][1]);
     // retuen key:value on the panel
     dataContainer.selectAll('p')
     .data(pair)
@@ -27,15 +26,70 @@ function buildMetadata(sample) {
       return `${d[0]}:${d[1]}`
     })
     
+    // BONUS: Build the Gauge Chart
+    var level = pair[5][1]*20;
+    // Trig to calc meter point
+   var degrees = 180 - level,
+   radius = .5;
+   var radians = degrees * Math.PI / 180;
+   var x = radius * Math.cos(radians);
+   var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+   var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+   pathX = String(x),
+   space = ' ',
+   pathY = String(y),
+   pathEnd = ' Z';
+   var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+   var data = [{ type: 'scatter',
+   x: [0], y:[0],
+   marker: {size: 28, color:'850000'},
+   showlegend: false,
+   name: 'speed',
+   text: level,
+   hoverinfo: 'text+name'},
+   { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+    rotation: 90,
+   text: ['8-9', '7-8', '6-7', '5-6',
+       '4-5', '3-4','2-3', '1-2', '0-1', ''],
+   textinfo: 'text',
+   textposition:'inside',
+   marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(41, 141, 28, .5)',
+                    'rgba(68, 155, 56, .5)', 'rgba(94, 169, 85, .5)',
+                    'rgba(121, 183, 115, .5)', 'rgba(147, 198, 141, .5)',
+                    'rgba(174, 221, 170, .5)', 'rgba(201, 226, 198, .5)', 
+                    'rgba(228, 240, 226, .5)', 'rgba(255, 255, 255, 0)']},
+   labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4','2-3', '1-2', '0-1' , ''],
+   hoverinfo: 'label',
+   hole: .5,
+   type: 'pie',
+   showlegend: false
+   }];
+
+   var layout = {
+     shapes:[{
+     type: 'path',
+     path: path,
+     fillcolor: '850000',
+     line: {
+       color: '850000'
+     }
+    }],
+     height: 300,
+     width: 300,
+     title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week', 
+     xaxis: {zeroline:false, showticklabels:false,
+        showgrid: false, range: [-1, 1]},
+     yaxis: {zeroline:false, showticklabels:false,
+        showgrid: false, range: [-1, 1]}
+    };
+
+     Plotly.newPlot('gauge', data, layout);
 
   });
-  // @TODO: Complete the following function that builds the metadata panel
-    
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  
 }
 
 function buildCharts(sample) {
@@ -43,7 +97,6 @@ function buildCharts(sample) {
   // Use `d3.json` to fetch the sample data for the plots
   d3.json(url2).then(function(data){
     var data = [data];
-    console.log(data);
     otuIds = data[0].otu_ids;
     otuLabels = data[0].otu_labels;
     sampleValues = data[0].sample_values
@@ -57,6 +110,7 @@ function buildCharts(sample) {
     var data1 = [trace1];
     Plotly.newPlot("pie", data1);
 
+    // Build a Bubble Chart using the sample data
     var trace2 ={
       x : otuIds,
       y : sampleValues,
@@ -73,14 +127,6 @@ function buildCharts(sample) {
 
   })
   
-
-  
-
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
 }
 
 function init() {
