7773889511cb56e19cd98966c56f699cd22036f5
chienyuanhung <chien.yuanhung@gmail.com>
2019-08-03 15:27:36 -0700

    put gauge chart functionin bonus.js
    update bonus.js and README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
+# Belly Button Biodiversity
+
 [Web app deployed through Heroku](https://belly-button-biodiversi.herokuapp.com)
+
+* web page for belly button biodiversity data
+ 
+![](images/wbshow.png)
+
+* dropdown menu for data
+
+![](images/dropdown.png)
+
+* info chart
+
+![](images/info_table.png)
+
+* pie chart for top ten samples
+
+![](images/pie_chart.png)
+
+* guage chart showing washing frequency
+
+![](images/gauge.png)
+
+* bubble chart for all samples
+
+![](images/bubble_chart.png)
--- static/js/app.js
+++ static/js/app.js
@@ -26,70 +26,11 @@ function buildMetadata(sample) {
       return `${d[0]}:${d[1]}`;
     });
     
-    // BONUS: Build the Gauge Chart
-    var level = pair[5][1]*20;
-    // Trig to calc meter point
-   var degrees = 180 - level,
-   radius = .5;
-   var radians = degrees * Math.PI / 180;
-   var x = radius * Math.cos(radians);
-   var y = radius * Math.sin(radians);
-
-    // Path: may have to change to create a better triangle
-   var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-   pathX = String(x),
-   space = ' ',
-   pathY = String(y),
-   pathEnd = ' Z';
-   var path = mainPath.concat(pathX,space,pathY,pathEnd);
-
-   var data = [{ type: 'scatter',
-   x: [0], y:[0],
-   marker: {size: 28, color:'850000'},
-   showlegend: false,
-   name: 'speed',
-   text: level,
-   hoverinfo: 'text+name'},
-   { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
-    rotation: 90,
-   text: ['8-9', '7-8', '6-7', '5-6',
-       '4-5', '3-4','2-3', '1-2', '0-1', ''],
-   textinfo: 'text',
-   textposition:'inside',
-   marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(41, 141, 28, .5)',
-                    'rgba(68, 155, 56, .5)', 'rgba(94, 169, 85, .5)',
-                    'rgba(121, 183, 115, .5)', 'rgba(147, 198, 141, .5)',
-                    'rgba(174, 221, 170, .5)', 'rgba(201, 226, 198, .5)', 
-                    'rgba(228, 240, 226, .5)', 'rgba(255, 255, 255, 0)']},
-   labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4','2-3', '1-2', '0-1' , ''],
-   hoverinfo: 'label',
-   hole: .5,
-   type: 'pie',
-   showlegend: false
-   }];
-
-   var layout = {
-     shapes:[{
-     type: 'path',
-     path: path,
-     fillcolor: '850000',
-     line: {
-       color: '850000'
-     }
-    }],
-     height: 700,
-     width: 700,
-     title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week', 
-     xaxis: {zeroline:false, showticklabels:false,
-        showgrid: false, range: [-1, 1]},
-     yaxis: {zeroline:false, showticklabels:false,
-        showgrid: false, range: [-1, 1]}
-    };
-
-     Plotly.newPlot('gauge', data, layout);
-
+    // set level for gauge plot
+   var level = pair[5][1]*20;
+    // call gauge plot
+    gaugeChart(level);
   });
-  
 }
 
 function buildCharts(sample) {
@@ -105,7 +46,9 @@ function buildCharts(sample) {
     var trace1={
       labels: otuIds.slice(0, 10),
       values: sampleValues.slice(0, 10), 
+      textinfo: 'values', 
       text: otuLabels.slice(0, 10),
+      hoverinfo: 'text',
       type: 'pie'
     };
     var data1 = [trace1];
@@ -122,9 +65,12 @@ function buildCharts(sample) {
         color: otuIds
       }
     };
-
+    
+    var layout2 ={
+      xaxis: { title: "OTU ID" },
+    };
     var data2 = [trace2];
-    Plotly.newPlot("bubble", data2);
+    Plotly.newPlot("bubble", data2, layout2);
 
 
   });
